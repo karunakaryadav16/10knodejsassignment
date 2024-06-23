0ec27a38dab706a4cf154aa8387389fa0a84e169
karunakaryadav16 <124349022+karunakaryadav16@users.noreply.github.com>
2024-06-23 13:53:05 +0530

    Update server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -29,44 +29,9 @@ db.connect((err) => {
 });
 
 
-// app.post("/api/formdata", (req, res) => {
-//     console.log(req.body);
-   
-  
-//     let query = `
-//       insert into invoice_amount_details_table(
-//         currency,
-//         inv_basic_amount,
-//         inv_tax_amount,
-//         total_inv_amount,
-//         advance_paid,
-//         tcs_applicable,
-//         net_payable_amount
-//       )
-//       values(?, ?, ?, ?, ?, ?, ?)
-//     `;
-  
 
-// db.query(
-//       query,
-//       [
-//         currency,
-//         invBasicAmount,
-//         invTaxAmount,
-//         totalInvAmount,
-//         advancePaid,
-//         tcsApplicable,
-//         netPayableAmount,
-//       ],
-//       (err, results) => {
-//         if (err) {
-//           res.send("Error occurred while inserting the data");
-//         } else {
-//           res.send("Data sent successfully");
-//         }
-//       }
-//     );
-//   });
+
+
 
 
 
@@ -95,26 +60,7 @@ app.post('/api/formdata', (req, res) => {
     reference
   } = req.body;
 
-  // SQL query to insert data into the table
-  // const query = `
-  //   INSERT INTO invoice_amount_details_table (
-  //     currency,
-  //     inv_basic_amount,
-  //     inv_tax_amount,
-  //     total_inv_amount,
-  //     advance_paid,
-  //     tcs_applicable,
-  //     net_payable_amount,
-  //     alternate_payee1,
-  //     alternate_payee2,
-  //     city,
-  //     street,
-  //     country,
-  //     bank_key,
-  //     bank_acc_no,
-  //     reference
-  //   ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  // `;
+
 
 
 
@@ -171,11 +117,7 @@ app.post('/api/formdata', (req, res) => {
   });
 });
 
-// Start the server
-// const PORT = process.env.PORT || 3003;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+
 
 
 
